74e6856f3d5b8602ccbf9aeb5fc07f704a689c4b
Zazie <mvannozzi91@gmail.com>
2020-08-20 15:21:36 +0200

    Dettagli Cliente (modifica)

--- src/main/java/it/nike/gestnike/services/gestione_clienti/GestioneClienteService.java
+++ src/main/java/it/nike/gestnike/services/gestione_clienti/GestioneClienteService.java
@@ -17,7 +17,7 @@ public class GestioneClienteService {
     private ClienteRepository clienteRepository;
 
     /**
-     * 
+     *
      * @param ragSociale
      * @return
      * @throws Exception
